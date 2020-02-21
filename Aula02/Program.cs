399c5cf32c4f1d9b407f273a4b8d919202c88471
Reginaldo Viana <reginaldo_stz@outlook.com>
2020-02-21 19:57:42 -0300

    Curso de Cshap inicial

---
+++ Aula02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Arrays
+           var nomes = new string[]{
+               "Reginaldo",
+               "Carlos",
+               "Rafael",
+               "Erick",
+               "Joel",
+               "Fernando",
+           };
+
+           foreach (var nome in nomes){
+               Console.WriteLine(nome);
+           }*/
+
+
+            //List
+           var cidades = new List<string>{
+               "SP",
+               "RP",
+               "JB"
+           };
+
+           cidades.Add("TQ");
+
+           foreach( var cidade in cidades){
+               Console.WriteLine(cidade);
+           }
+            
+        }
+    }
+}
---
+++ FessorBruno/Aula02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+           var nomes = new string[]{
+               "Reginaldo",
+               "Carlos",
+               "Rafael",
+               "Erick",
+               "Joel",
+               "Fernando",
+           };
+
+           foreach (var nome in nomes){
+               Console.WriteLine(nome);
+           }*/
+
+           var cidades = new List<string>{
+               "SP",
+               "RP",
+               "JB"
+           };
+
+           cidades.Add("TQ");
+
+           foreach( var cidade in cidades){
+               Console.WriteLine(cidade);
+           }
+            
+        }
+    }
+}
